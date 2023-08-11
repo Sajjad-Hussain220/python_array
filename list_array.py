9a117186a17055a184b1470f20ae2aa4d569fa27
Sajjad-Hussain220 <narejo220@gmail.com>
2023-08-11 19:50:53 +0500

    python_array

---
+++ list_array.py
@@ -0,0 +1,90 @@
+# Create a list of juices, add 5 items using append
+
+# juices = []
+# juices.append("apple")
+# juices.append("orange")
+# juices.append("mango")
+# juices.append("grape")
+# juices.append("red apple")
+#
+# print(juices)
+
+# -------///-----------///-----------///-------
+
+# Create a list of cars, add 5 items using insert
+#
+# car = []
+# car.insert(0, "Toyota")
+# car.insert(1, "Honda")
+# car.insert(2, "corolla")
+# car.insert(3, "civic")
+# car.insert(4, "BMW")
+#
+# print(car)
+
+# -------///-----------///-----------///-------
+
+# Remove last item from the list [‘bed’, ‘table’, ‘chair’, ‘sofa’]	using pop and len
+#
+# list = ['bed', 'table', 'chair', 'sofa']
+# list.pop()
+# print(list)
+
+# -------///-----------///-----------///-------
+
+# Remove second last item from the list [‘bed’, ‘table’, ‘chair’, ‘sofa’] by len and index
+#
+# list = ['bed', 'table', 'chair', 'sofa']
+# list.pop(2)
+# print(list)
+
+# -------///-----------///-----------///-------
+
+# Remove first item from the list [‘bed’, ‘table’, ‘chair’, ‘sofa’]
+#
+# list = ['bed', 'table', 'chair', 'sofa']
+# list.pop(0)
+# print(list)
+
+# -------///-----------///-----------///-------
+
+# Remove the item “chair” from the list [‘bed’, ‘table’, ‘chair’, ‘sofa’]
+
+# list = ['bed', 'table', 'chair', 'sofa']
+# list.remove('chair')
+# print(list)
+
+
+# -------///-----------///-----------///-------
+
+# Remove all “chair” from the list [‘bed’, ‘table’, ‘chair’, ‘sofa’, ‘chair’]
+#
+# list = ['bed', 'table', 'chair', 'sofa', 'chair']
+# list.remove('chair')
+# list.remove('chair')
+# print(list)
+
+# -------///-----------///-----------///-------
+
+# # Merge two lists [‘A’, “B”, “C”] and [“D”, “E”, “F”]
+#
+# list_1 = ['A', 'B', 'C']
+# list_2 = ['D', 'E', 'F']
+#
+# add = list_1 + list_2
+# print(add)
+
+
+# -------///-----------///-----------///-------
+
+# Write a Python program to check if a list is empty or not.
+#
+# list_1 = []
+# list_2 = [1, 'a', 2 ]
+#
+# if len(list_1) == 0:
+#     print(f"{list_1} list is empty")
+# if len(list_2) >= 0:
+#     print(f"{list_2} list is not empty")
+#
+
